f1cb62fa1434e783c049896dcd715f801d380c83
Alexandra Romano <alex.romano80@gmail.com>
2022-11-22 12:37:46 -0700

    commit tree 1738da966d907730521ac9e5bf1efafc12eae5e2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, string){
+    const newArray =[];
+    for (let i=0; i < names.length; i++) {
+  newArray.push(`Thank you, ${names[i]}, for the wonderful ${string} gift!`)
+    }
+    return newArray;
+}
+
+    function countDown(newYears) {
+        while(newYears >= 0) {
+           console.log(newYears); 
+           newYears--;
+        }
+    } 
